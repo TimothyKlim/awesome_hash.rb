77b093a51609ff3f27b620c41f5bb9511aad3490
Timothy Klim <klimtimothy@gmail.com>
2013-05-26 19:09:29 +0400

    Initial Commit

---
+++ ulib_gem/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ulib_gem
+//
+//  Created by Timothy Klim on 5/26/13.
+//  Copyright (c) 2013 Timothy Klim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
